7e34831b9dab91aac3d5927951f2920b95f49842
louizimoncef <moncefloui@gmail.com>
2021-04-11 18:36:45 +0000

    upp

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -2,9 +2,9 @@
 uint8_t *sha256(int8_t const *s, size_t len,
 uint8_t digest[SHA256_DIGEST_LENGTH])
 {
-unsigned char *testptr=NULL;
-if (strlen((char*)s) > 0)
-testptr=SHA256((unsigned char*)s, len, digest);
-return (testptr !=NULL ? digest : NULL);
+unsigned char *testptr = NULL;
+if (strlen((char *)s) > 0)
+testptr = SHA256((unsigned char*)s, len, digest);
+return (testptr != NULL ? digest : NULL);
 }
 
