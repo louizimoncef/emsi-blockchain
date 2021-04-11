48c22070d622fb0ffccc00187d6cfa0f1b309ae0
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-11 19:36:04 +0000

    Update sha256.c

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -2,18 +2,12 @@
 uint8_t *sha256(int8_t const *s, size_t len,
 uint8_t digest[SHA256_DIGEST_LENGTH])
 {
-if (s)
+if (strlen((char*)s) > 0)
 {
-unsigned int mt_len;
-SHA256_CTX *mdctx;
-const EVP_MD *md;
-md = EVP_sha256();
-mdctx =(SHA256_CTX *) EVP_MD_CTX_create();
-EVP_DigestInit_ex((SHA256_CTX *)mdctx, md, NULL);
-EVP_DigestUpdate((SHA256_CTX *)mdctx, s, len);
-EVP_DigestFinal_ex((SHA256_CTX *)mdctx, digest, &mt_len);
-EVP_MD_CTX_destroy((SHA256_CTX *)mdctx);
-EVP_cleanup();
+SHA256_CTX ctx;
+SHA256_Init(&ctx);
+SHA256_Update(&ctx, s, len);
+SHA256_Final(digest, &ctx);
 }
-return (digest ? digest : NULL);
+return (digest != NULL ? digest : NULL);
 }
