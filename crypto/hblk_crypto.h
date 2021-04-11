9cc0c275e5a4f5ae96ee05975bf4b09fa220379a
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-11 13:59:58 +0000

    Update hblk_crypto.h

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -3,6 +3,6 @@
 #include<stdint.h>
 #include<inttypes.h>
 #include <openssl/ssl.h>
-uint8_t *sha256(int8_t const *s, unsigned int len,
+uint8_t *sha256(int8_t const *s, size_t len,
 uint8_t digest[SHA256_DIGEST_LENGTH]);
 #endif
