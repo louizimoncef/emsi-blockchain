d291348e186edf26b5bb29da6bce1cc098605b18
louizimoncef <moncefloui@gmail.com>
2021-04-11 13:55:53 +0000

    sha256

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -1,6 +1,8 @@
 #ifndef hblk_crypto
 #define hblk_crypto
+#include<stdint.h>
+#include<inttypes.h>
 #include <openssl/ssl.h>
-uint8_t *sha256(int8_t const *s, size_t len,
+uint8_t *sha256(int8_t const *s, unsigned int len,
 uint8_t digest[SHA256_DIGEST_LENGTH]);
 #endif
---
+++ crypto/provided/_print_hex_buffer.c
@@ -0,0 +1,9 @@
+#include "hblk_crypto.h"
+void _print_hex_buffer(uint8_t const *buf, size_t len) 
+{
+size_t i;
+for (i = 0; i < len; i++)
+printf("%02x", buf[i]);
+printf("\n");
+}
+
