1726afcbc94e8a4be2c028f2c12f525059cd74c1
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-11 18:45:04 +0000

    Update sha256.c

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -2,11 +2,15 @@
 uint8_t *sha256(int8_t const *s, size_t len,
 uint8_t digest[SHA256_DIGEST_LENGTH])
 {
-unsigned char *testptr = NULL;
-if (strlen((char *)s) > 0)
-testptr = SHA256((unsigned char*)s, len, digest);
-if (testptr != NULL )        
-return  digest;
-return NULL;
+SHA256_CTX context;
+
+	SHA256_Init(&context);
+	SHA256_Update(&context, s, len);
+	SHA256_Final(digest ,&context);
+	
+	if(!digest)
+		return NULL;
+	
+	return digest;
 }
 
