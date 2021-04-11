277aac44bbf6e2f5bffefed40acfe78f3c905108
louizimoncef <moncefloui@gmail.com>
2021-04-11 16:40:10 +0000

    up

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -6,3 +6,4 @@
 uint8_t *sha256(int8_t const *s, size_t len,
 uint8_t digest[SHA256_DIGEST_LENGTH]);
 #endif
+
--- crypto/sha256.c
+++ crypto/sha256.c
@@ -2,7 +2,6 @@
 uint8_t *sha256(int8_t const *s, size_t len,
 uint8_t digest[SHA256_DIGEST_LENGTH])
 {
-if (s)
 {
 unsigned int md_len;
 EVP_MD_CTX *mdctx;
@@ -14,7 +13,6 @@ EVP_DigestUpdate(mdctx, s, len);
 EVP_DigestFinal_ex(mdctx, digest, &md_len);
 EVP_MD_CTX_destroy(mdctx);
 EVP_cleanup();
-}
 return (digest ? digest : NULL);
 }
 
