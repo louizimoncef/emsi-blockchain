ab781ac7434b0fff5581875979154a6b2a3bdcfb
louizimoncef <moncefloui@gmail.com>
2021-04-11 15:52:09 +0000

    up

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -1,18 +1,20 @@
 #include "hblk_crypto.h"
-uint8_t *sha256(int8_t const *s, unsigned int len,
+uint8_t *sha256(int8_t const *s, size_t len,
 uint8_t digest[SHA256_DIGEST_LENGTH])
 {
 if (s)
 {
 EVP_MD_CTX *mdctx;
 const EVP_MD *md;
+unsigned int md_len;
 md = EVP_sha256();
 mdctx = EVP_MD_CTX_create();
 EVP_DigestInit_ex(mdctx, md, NULL);
-EVP_DigestUpdate(mdctx, s, strlen((char *)s));
-EVP_DigestFinal_ex(mdctx, digest, &len);
+EVP_DigestUpdate(mdctx, s, len);
+EVP_DigestFinal_ex(mdctx, digest, &md_len);
 EVP_MD_CTX_destroy(mdctx);
 EVP_cleanup();
+len = (int)md_len;
 }
 return (digest ? digest : NULL);
 }
