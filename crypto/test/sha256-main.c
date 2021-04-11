3b0d86881dfa6407722dd92cc31277ec11ecaed2
louizimoncef <moncefloui@gmail.com>
2021-04-11 17:02:34 +0000

    up

--- crypto/test/sha256-main.c
+++ crypto/test/sha256-main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include "hblk_crypto.h"
-void _print_hex_buffer(uint8_t const *buf, size_t len);
 int main(int ac, char **av)
 {
 uint8_t hash[SHA256_DIGEST_LENGTH];
@@ -23,8 +22,7 @@ if (test_ptr != hash)
 fprintf(stderr, "Return value and pointer differ\n");
 return (EXIT_FAILURE);
 }
-printf("\"%s\" hash is: ", av[1]);
 _print_hex_buffer(hash, SHA256_DIGEST_LENGTH);
-printf("\n");
 return (EXIT_SUCCESS);
 }
+
