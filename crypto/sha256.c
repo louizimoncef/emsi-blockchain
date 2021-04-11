d529546df604220fbfe65986cf749d3b478617fa
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-11 18:04:18 +0000

    Update sha256.c

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -2,13 +2,13 @@
 uint8_t *sha256(int8_t const *s, size_t len,
 uint8_t digest[SHA256_DIGEST_LENGTH])
 {
+digest = NULL;
 if (s)
 {
 SHA256_CTX ctx;
 SHA256_Init(&ctx);
 SHA256_Update(&ctx, s, len);
-SHA256_Final(digest,&ctx);
-return digest;
+SHA256_Final(digest, &ctx);
 }
- return NULL;
+return (digest ? digest : NULL);
 }
