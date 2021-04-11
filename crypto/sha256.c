c7dc85ab086392efc19914dbfbd4269f79daeb51
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-11 19:44:20 +0000

    Update sha256.c

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -1,10 +1,9 @@
 #include "hblk_crypto.h"
-uint8_t * sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]){
-        SHA256_CTX ctx;
-        if(!digest)
-                return NULL;
-        SHA256_Init(&ctx);
-        SHA256_Update(&ctx, s, len);
-        SHA256_Final(digest,&ctx);
-        return digest;
+uint8_t *sha256(int8_t const *s, size_t len,
+uint8_t digest[SHA256_DIGEST_LENGTH])
+{
+unsigned char *testptr = NULL;
+if (strlen((char *)s) > 0)
+testptr = SHA256((unsigned char *)s, len, digest);
+return (testptr != NULL ? digest : NULL);
 }
