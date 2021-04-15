8cd066725aea9347a0e0b1c45582f468eb85d7cc
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-15 01:23:09 +0000

    Update hblk_crypto.h

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -3,6 +3,7 @@
 #include<stdint.h>
 #include<inttypes.h>
 #include <openssl/ssl.h>
+#include <openssl/sha.h>
 #define EC_PUB_LEN 65
 uint8_t *sha256(int8_t const *s, size_t len,
 uint8_t digest[SHA256_DIGEST_LENGTH]);
