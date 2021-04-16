bbf1e367ace37dfa8244d06a90639de2d60b9bf6
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-16 17:36:39 +0000

    Update ec_to_pub.c

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -1,26 +1,22 @@
 #include "hblk_crypto.h"
-uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
-{
-const EC_POINT *ecp=EC_KEY_get0_public_key(key);
-const EC_GROUP* ec_group_new = EC_KEY_get0_group(key);
-BN_CTX* bnctx = BN_CTX_new();
-if (ecp == NULL)
-return NULL;
 
-if (ec_group_new==NULL)
-{
-EC_POINT_free(ecp);
-return NULL;
-}
-if (bnctx == NULL)
-{
-EC_POINT_free(ecp);
-EC_GROUP_free(ec_group_new);
-return NULL;
-}
-int test = EC_POINT_point2oct(ec_group_new, ecp, POINT_CONVERSION_UNCOMPRESSED, pub, EC_PUB_LEN, bnctx);
-EC_POINT_free(ecp);
-EC_GROUP_free(ec_group_new);
-BN_CTX_free(bnctx);
-return test?pub:NULL;
+
+uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]){
+
+	const EC_GROUP *group;
+	const EC_POINT *point;
+
+
+	if(!key || !pub) return NULL;
+
+
+	group = EC_KEY_get0_group (key);
+	point = EC_KEY_get0_public_key (key);
+	
+	if(!EC_POINT_point2oct(group, point,
+				 POINT_CONVERSION_UNCOMPRESSED, pub, EC_PUB_LEN, NULL))
+		return NULL;
+
+	return pub;
+	
 }
