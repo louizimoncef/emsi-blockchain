dfe326afcf562a9f153f27951ba312e54086329e
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-16 18:09:59 +0000

    Update ec_to_pub.c

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -1,10 +1,12 @@
 #include "hblk_crypto.h"
+
 /**
  * ec_to_pub - convert ec key to public key
  * @*key: pointer to ec key
  * @pub[EC_PUB_LEN]: where we stock public key
  * return: public key or NULL
  */
+
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 {
 const EC_GROUP *group;
