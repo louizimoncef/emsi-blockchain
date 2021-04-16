3eaf9d2480f0ea85f81fb7226a519b7e1e079be9
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-16 17:44:04 +0000

    Update ec_create.c

--- crypto/ec_create.c
+++ crypto/ec_create.c
@@ -2,7 +2,7 @@
 EC_KEY *ec_create(void)
 {
 EC_KEY *ec_key = EC_KEY_new();
-EC_GROUP *ec_group_new = EC_GROUP_new_by_curve_name(NID_secp256k1);
+EC_GROUP *ec_group_new = EC_GROUP_new_by_curve_name(EC_CURVE);
 const EC_GROUP *ec_group = ec_group_new;
 if (!EC_KEY_set_group(ec_key, ec_group))
 return (NULL);
