0943000118b7cf2f877a0fa0f39e403cd263e1f2
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-17 04:06:02 +0000

    Update hblk_crypto.h

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -6,6 +6,8 @@
 #include <openssl/sha.h>
 #define EC_PUB_LEN 65
 #define EC_CURVE	NID_secp256k1
+#define PRK_FN "key.pem"
+#define PUK_FN "key_pub.pem"
 uint8_t *sha256(int8_t const *s, size_t len,
 uint8_t digest[SHA256_DIGEST_LENGTH]);
 EC_KEY *ec_create(void);
