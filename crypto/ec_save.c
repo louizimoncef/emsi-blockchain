4aa29809b32417422314a13135bf3d02724753db
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-17 03:50:53 +0000

    Update ec_save.c

--- crypto/ec_save.c
+++ crypto/ec_save.c
@@ -2,30 +2,35 @@
 
 int ec_save(EC_KEY *key, char const *folder)
 {
-	char file[512];
-	FILE *f;
-	struct stat st;
-
-	if (!key || !folder)
+char PRK_FN[] = "/key.pem", PUK_FN[] = "/key_pub.pem",
+*PRK_dest = NULL, *PUK_dest = NULL;
+int const PRK_DEST_LEN = strlen(PRK_FN) + strlen(folder) + 1,
+PUK_DEST_LEN = strlen(PUK_FN) + strlen(folder) + 1;
+FILE *PRK_F = NULL, *PUK_F = NULL;
+PUK_dest = (char *) malloc(PUK_DEST_LEN);
+PRK_dest = (char *) malloc(PRK_DEST_LEN);
+struct stat st;
+if (!key)
+return (0);
+if (stat(folder, &st) == -1)
+{
+	if (mkdir(folder, 0700) == -1)
 		return 0;
-	if (stat(folder, &st) == -1)
-	{
-		if (mkdir(folder, 0700) == -1)
-			return 0;
-	}
-	sprintf(file, "%s/%s", folder, PRI_FILENAME);
-	f = fopen(file, "w");
-	if (!f)
-		return 0;
-	if (!PEM_write_ECPrivateKey(f, key, NULL, NULL, 0, NULL, NULL))
-		return 0;
-	fclose(f);
-	sprintf(file, "%s/%s", folder, PUB_FILENAME);
-	f = fopen(file, "w");
-	if (!f)
-		return 0;
-	if (!PEM_write_EC_PUBKEY(f, key))
-		return 0;
-	fclose(f);
-	return 1;
+}
+if (!PUK_dest && !PRK_dest)
+{
+	printf("malloc error");
+	return (0);
+}
+PRK_F = fopen(PRK_dest, "w+");
+PUK_F = fopen(PUK_dest, "w+");
+if (PEM_write_ECPrivateKey(PRK_F, key, NULL, NULL, 0, NULL, NULL) && PEM_write_EC_PUBKEY(PUK_F, key) )
+{
+fclose(PRK_F);
+fclose(PUK_F);
+return (1);
+}
+printf("creating error");
+return (0);
+	
 }
