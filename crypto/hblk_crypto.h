c24a94b38bcf71bf1bab687ae80a284d3c60f7a2
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-17 04:33:05 +0000

    Update hblk_crypto.h

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -12,14 +12,10 @@
 #include <stdint.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-
 #define PUB_FILENAME	"key_pub.pem"
 #define PRI_FILENAME	"key.pem"
 #define EC_CURVE	NID_secp256k1
-
 #define EC_PUB_LEN	65
-#define SIG_MAX_LEN	72
-#define EC_PRIV_LEN 100
 uint8_t *sha256(int8_t const *s, size_t len,
 uint8_t digest[SHA256_DIGEST_LENGTH]);
 EC_KEY *ec_create(void);
