c7c66f029013742381c6d8e9c51b205ddf6e0b68
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-17 15:26:48 +0000

    Update ec_load.c

--- crypto/ec_load.c
+++ crypto/ec_load.c
@@ -1,29 +1,25 @@
 #include "hblk_crypto.h"
-
 EC_KEY *ec_load(char const *folder)
 {
-	EC_KEY *key = NULL;
-	char file[512] = {0};
-	FILE *f;
-	struct stat st;
-
-	if (!folder)
-		return NULL;
-	if (stat(folder, &st) == -1)
-		return NULL;
-	sprintf(file, "./%s/%s", folder, PUB_FILENAME);
-	f = fopen(file, "r");
-	if (!f)
-		return NULL;
-	if (!PEM_read_EC_PUBKEY(f, &key, NULL, NULL))
-		return NULL;
-	fclose(f);
-	sprintf(file, "./%s/%s", folder, PRI_FILENAME);
-	f = fopen(file, "r");
-	if (!f)
-		return NULL;
-	if (!PEM_read_ECPrivateKey(f, &key, NULL, NULL))
-		return NULL;
-	fclose(f);
-	return key;
+char file[512] = { 0 };
+FILE *f, *f1;
+EC_KEY* ec_key = NULL;
+struct stat st;
+if (!folder)
+return (NULL);
+if (stat(folder, &st) == -1)
+return (NULL);
+sprintf(file, "./%s/%s", folder, PUB_FILENAME);
+f = fopen(file, "r");
+sprintf(file, "./%s/%s", folder, PRI_FILENAME);
+f1 = fopen(file, "r");
+if (!f && !f1)
+return (NULL);
+if (!PEM_read_EC_PUBKEY(f, &ec_key, NULL, NULL))
+return (NULL);
+fclose(f);
+if (!PEM_read_ECPrivateKey(f1, &ec_key, NULL, NULL))
+return (NULL);
+fclose(f1);
+return (ec_key);
 }
