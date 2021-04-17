92ba3835cd975b4c2c4327b581e5658a3a3292aa
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-17 03:57:20 +0000

    Update ec_save.c

--- crypto/ec_save.c
+++ crypto/ec_save.c
@@ -9,12 +9,9 @@
 int ec_save(EC_KEY *key, char const *folder)
 {
 char PRK_FN[] = "/key.pem", PUK_FN[] = "/key_pub.pem",
-*PRK_dest = NULL, *PUK_dest = NULL;
+PRK_dest[600], *PUK_dest[600];
 int const PRK_DEST_LEN = strlen(PRK_FN) + strlen(folder) + 1,
 PUK_DEST_LEN = strlen(PUK_FN) + strlen(folder) + 1;
-FILE *PRK_F = NULL, *PUK_F = NULL;
-PUK_dest = (char *) malloc(PUK_DEST_LEN);
-PRK_dest = (char *) malloc(PRK_DEST_LEN);
 struct stat st;
 if (!key)
 return (0);
@@ -23,15 +20,11 @@ if (stat(folder, &st) == -1)
 	if (mkdir(folder, 0700) == -1)
 		return 0;
 }
-if (!PUK_dest && !PRK_dest)
+PRK_F = fopen(PRK_dest, "w");
+PUK_F = fopen(PUK_dest, "w");
+if (PEM_write_ECPrivateKey(PRK_F, key, NULL, NULL, 0, NULL, NULL)  )
 {
-	printf("malloc error");
-	return (0);
-}
-PRK_F = fopen(PRK_dest, "w+");
-PUK_F = fopen(PUK_dest, "w+");
-if (PEM_write_ECPrivateKey(PRK_F, key, NULL, NULL, 0, NULL, NULL) && PEM_write_EC_PUBKEY(PUK_F, key) )
-{
+PEM_write_EC_PUBKEY(PUK_F, key)
 fclose(PRK_F);
 fclose(PUK_F);
 return (1);
