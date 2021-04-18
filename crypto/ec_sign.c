a9a09df1acffbdeedcb7ceff1d73fab023b949a6
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-18 17:54:04 +0000

    Update ec_sign.c

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -1,6 +1,7 @@
 #include "hblk_crypto.h"
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t *sig)
 {
+memset(sig->sig, 0, sizeof(sig->sig));
 if (key && msg)
 if (ECDSA_sign(0, msg, msglen, sig->sig,(unsigned int*)&sig->len, (EC_KEY *)key))
 return (sig->sig);
