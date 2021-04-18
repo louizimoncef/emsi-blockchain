4c74f930c5f242eb11ec6392b1ee2b0412cc71dc
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-18 18:09:33 +0000

    Update ec_sign-main.c

--- crypto/test/ec_sign-main.c
+++ crypto/test/ec_sign-main.c
@@ -8,9 +8,9 @@ void _print_hex_buffer(uint8_t const *buf, size_t len);
 
 /**
  * test_ec_sign - Test the ec_sign function
- * 
+ *
  * @key: Pointer to the EC Key pair to use to sign the message
- * 
+ *
  * Return: EXIT_SUCCESS or EXIT_FAILURE
  */
 static int test_ec_sign(EC_KEY const *key)
@@ -59,48 +59,8 @@ int main(void)
         EC_KEY_free(key);
         return (EXIT_FAILURE);
     }
-    if (test_ec_sign(key) != EXIT_SUCCESS)
-    {
-        EC_KEY_free(key);
-        return (EXIT_FAILURE);
-    }
-    if (test_ec_sign(key) != EXIT_SUCCESS)
-    {
-        EC_KEY_free(key);
-        return (EXIT_FAILURE);
-    }
-        if (test_ec_sign(key) != EXIT_SUCCESS)
-    {
-        EC_KEY_free(key);
-        return (EXIT_FAILURE);
-    }
-    if (test_ec_sign(key) != EXIT_SUCCESS)
-    {
-        EC_KEY_free(key);
-        return (EXIT_FAILURE);
-    }
-        if (test_ec_sign(key) != EXIT_SUCCESS)
-    {
-        EC_KEY_free(key);
-        return (EXIT_FAILURE);
-    }
-    if (test_ec_sign(key) != EXIT_SUCCESS)
-    {
-        EC_KEY_free(key);
-        return (EXIT_FAILURE);
-    }
-        if (test_ec_sign(key) != EXIT_SUCCESS)
-    {
-        EC_KEY_free(key);
-        return (EXIT_FAILURE);
-    }
-    if (test_ec_sign(key) != EXIT_SUCCESS)
-    {
-        EC_KEY_free(key);
-        return (EXIT_FAILURE);
-    }
+
     /* Cleanup */
     EC_KEY_free(key);
 
     return (EXIT_SUCCESS);
-}
