7ef44a35c6f644b99d62a42d068520a0f4d55969
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-18 17:59:09 +0000

    Update ec_sign.c

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -3,7 +3,7 @@ uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t *si
 {
 memset(sig->sig, 0, sizeof(sig->sig));
 if (key && msg)
-if (ECDSA_sign(0, msg, msglen, sig->sig, &sig->len, key))
+if (ECDSA_sign(0, msg, msglen, sig->sig, (unsigned int *) &sig->len, (EC_KEY *) key))
 return (sig->sig);
 return (NULL);
 }
