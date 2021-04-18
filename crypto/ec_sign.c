34435e8d64eb5fde2a49d7535070518e0a2fc113
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-18 18:27:02 +0000

    Update ec_sign.c

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -6,7 +6,7 @@
  * @msg: message to be signed
  * @msglen: length of that message
  * @sig: pointer to  the signature
- * Return: pointer to the signature on sucess, NULL on failure
+ * Return: pointer to the signature on sucess NULL failure
  */
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
 size_t msglen, sig_t *sig)
