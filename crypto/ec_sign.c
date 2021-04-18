993c2b8a3bac5ebba6e353302671978d81eaf4b2
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-18 16:26:58 +0000

    Create ec_sign.c

---
+++ crypto/ec_sign.c
@@ -0,0 +1,8 @@
+#include "hblk_crypto.h"
+uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t *sig)
+{
+if (key && msg)
+if (ECDSA_sign(0, msg, msglen, sig->sig, &sig->len,key))
+return (sig->sig);
+return (NULL);
+}
