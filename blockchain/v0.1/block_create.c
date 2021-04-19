5e6406a4a704d6fcad185d57fc238fba8ed765c2
louizimoncef <moncefloui@gmail.com>
2021-04-19 23:34:35 +0000

    up

--- blockchain/v0.1/block_create.c
+++ blockchain/v0.1/block_create.c
@@ -10,8 +10,6 @@ block_t *block_create(block_t const *prev, int8_t const *data,
 uint32_t data_len)
 {
 block_t *block = (block_t *) malloc(sizeof(block_t));
-if (!prev || !data)
-return (NULL);
 block->info.index = prev->info.index + 1;
 block->info.difficulty = 0;
 block->info.nonce = 0;
