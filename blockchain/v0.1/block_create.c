5e0be1c79fc30e77a2c394bad598ecb00a8c2fe0
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-19 23:45:48 +0000

    Update block_create.c

--- blockchain/v0.1/block_create.c
+++ blockchain/v0.1/block_create.c
@@ -1,42 +1,28 @@
 #include "blockchain.h"
 /**
- * block_create - creates a new block
- * @prev: previous block
- * @data: data for a new block
- * @data_len: length of @data
- * Return: a block or NULL if failed
+ * block_create - create a block and initialises it
+ *@prev: previous  block
+ *@data: data to be stored in the block
+ *@data_len: length of data
+ * Return: a new block structure or NULL
  */
-block_t *block_create(block_t const *prev,
-		      int8_t const *data, uint32_t data_len)
+block_t *block_create(block_t const *prev, int8_t const *data,
+uint32_t data_len)
 {
-block_t *block;
-time_t t;
-int i;
-uint32_t max_len = data_len > BLOCKCHAIN_DATA_MAX ?
-BLOCKCHAIN_DATA_MAX : data_len;
-if (prev == NULL)
+block_t *block = (block_t *) malloc(sizeof(block_t));
+uint32_t i;
+if (!prev || !data)
 return (NULL);
-if (data == NULL)
-return (NULL);
-block = (block_t *)malloc(sizeof(block_t));
-block->info.index = (uint32_t)((int)(prev->info.index) + 1);
+block->info.index = prev->info.index + 1;
 block->info.difficulty = 0;
-t = time(0);
-block->info.timestamp = (uint64_t)&t;
 block->info.nonce = 0;
-for (i = 0; i < BLOCKCHAIN_DATA_MAX; i++)
-{
-block->data.buffer[i] = 0;
-}
-for (i = 0; i < (int)max_len; i++)
-{
-block->data.buffer[i] = data[i];
-}
-block->data.len = max_len;
-for (i = 0; i < SHA256_DIGEST_LENGTH; i++)
-{
-block->hash[i] = 0;
-block->info.prev_hash[i] = prev->hash[i];
-}
+block->info.timestamp = time(0);
+memcpy(block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
+memcpy(block->data.buffer, data, BLOCKCHAIN_DATA_MAX);
+if (data_len > (uint32_t)BLOCKCHAIN_DATA_MAX)
+block->data.len = BLOCKCHAIN_DATA_MAX;
+else
+block->data.len = data_len;
+block->hash[0] = 0;
 return (block);
 }
