77bb5a3c8d2276fc869bd2dbd26a729fc423e581
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-19 01:09:29 +0000

    Update ec_sign-main.c

--- crypto/test/ec_sign-main.c
+++ crypto/test/ec_sign-main.c
@@ -64,7 +64,81 @@ int main(void)
         EC_KEY_free(key);
         return (EXIT_FAILURE);
     }
-
+            if (test_ec_sign(key) != EXIT_SUCCESS)
+    {
+        EC_KEY_free(key);
+        return (EXIT_FAILURE);
+    }
+            if (test_ec_sign(key) != EXIT_SUCCESS)
+    {
+        EC_KEY_free(key);
+        return (EXIT_FAILURE);
+    }
+            if (test_ec_sign(key) != EXIT_SUCCESS)
+    {
+        EC_KEY_free(key);
+        return (EXIT_FAILURE);
+    }
+            if (test_ec_sign(key) != EXIT_SUCCESS)
+    {
+        EC_KEY_free(key);
+        return (EXIT_FAILURE);
+    }        if (test_ec_sign(key) != EXIT_SUCCESS)
+    {
+        EC_KEY_free(key);
+        return (EXIT_FAILURE);
+    }
+            if (test_ec_sign(key) != EXIT_SUCCESS)
+    {
+        EC_KEY_free(key);
+        return (EXIT_FAILURE);
+    }        if (test_ec_sign(key) != EXIT_SUCCESS)
+    {
+        EC_KEY_free(key);
+        return (EXIT_FAILURE);
+    }        if (test_ec_sign(key) != EXIT_SUCCESS)
+    {
+        EC_KEY_free(key);
+        return (EXIT_FAILURE);
+    }        if (test_ec_sign(key) != EXIT_SUCCESS)
+    {
+        EC_KEY_free(key);
+        return (EXIT_FAILURE);
+    }
+            if (test_ec_sign(key) != EXIT_SUCCESS)
+    {
+        EC_KEY_free(key);
+        return (EXIT_FAILURE);
+    }
+            if (test_ec_sign(key) != EXIT_SUCCESS)
+    {
+        EC_KEY_free(key);
+        return (EXIT_FAILURE);
+    }
+            if (test_ec_sign(key) != EXIT_SUCCESS)
+    {
+        EC_KEY_free(key);
+        return (EXIT_FAILURE);
+    }
+            if (test_ec_sign(key) != EXIT_SUCCESS)
+    {
+        EC_KEY_free(key);
+        return (EXIT_FAILURE);
+    }
+            if (test_ec_sign(key) != EXIT_SUCCESS)
+    {
+        EC_KEY_free(key);
+        return (EXIT_FAILURE);
+    }
+            if (test_ec_sign(key) != EXIT_SUCCESS)
+    {
+        EC_KEY_free(key);
+        return (EXIT_FAILURE);
+    }        if (test_ec_sign(key) != EXIT_SUCCESS)
+    {
+        EC_KEY_free(key);
+        return (EXIT_FAILURE);
+    }
     /* Cleanup */
     EC_KEY_free(key);
 
