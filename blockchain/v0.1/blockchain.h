0a4eae800d06d8fb9c85aa9111f258b38f6e07a1
louizimoncef <moncefloui@gmail.com>
2021-04-19 21:07:10 +0000

    up

--- blockchain/v0.1/blockchain.h
+++ blockchain/v0.1/blockchain.h
@@ -9,7 +9,21 @@
 #include <openssl/sha.h>
 #include <unistd.h>
 #include "../../crypto/hblk_crypto.h"
-
+#define GENESIS_BLOCK { \
+{ /* info */ \
+0 /* index */, \
+0, /* difficulty */ \
+1537578000, /* timestamp */ \
+0, /* nonce */ \
+{0} /* prev_hash */ \
+}, \
+{ /* data */ \
+"Holberton School", /* buffer */ \
+16 /* len */ \
+}, \
+"\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d" \
+"\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03" \
+}
 
 
 typedef struct blockchain_s
--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -1,59 +1,18 @@
 #include "blockchain.h"
 
-
-blockchain_t *blockchain_create(void){
-	blockchain_t *blockchain;
-	block_t *genesis;
-
-	blockchain = malloc(sizeof(*blockchain));
-
-
-	if (blockchain == NULL) return NULL;
-
-	/* Creating genesis block  with prefixed values */
-
-	char data[] =  "Holberton School";
-	char hash[] = "c52c26c8b5461639635d8edf2a97d48d0c8e0009c817f2b1d3d7ff2f04515803";
-	uint8_t len = strlen(data);
-	int _timestamp =	1537578000;
-	
-
-	genesis = (block_t*)malloc(sizeof(*genesis));
-
-	if (genesis == NULL) return NULL;
-
-	memcpy(genesis->data.buffer, data, len);
-	genesis->data.len = len;
-	// genesis->data.buffer[len] = '\0'
-	*(genesis->data.buffer + len) = '\0';
-
-	memcpy(genesis->hash, hash, SHA256_DIGEST_LENGTH);
-	genesis->info.timestamp = _timestamp;
-
-
-
-	if (genesis == NULL){
-
-		free(blockchain);
-		return NULL;
-	}
-
-
-	blockchain->chain = llist_create(MT_SUPPORT_TRUE);
-
-	if (blockchain->chain == NULL){
-		free(blockchain);
-		free(genesis);
-		return NULL;
-	}
-
-	if (llist_add_node(blockchain->chain, genesis, ADD_NODE_FRONT) == -1){
-
-		llist_destroy(blockchain->chain, 1, NULL);
-
-		free(blockchain);
-		free(genesis);
-		return NULL;
-	}
-	return blockchain;
+/**
+ * blockchain_create - create a block chain with genesis
+ * Return: a new blockchain structure
+ */
+blockchain_t *blockchain_create(void)
+{
+block_t gene = GENESIS_BLOCK, *genesis;
+blockchain_t *blockchain;
+genesis = (block_t *) malloc(sizeof(block_t));
+memcpy(genesis, (block_t *) &gene, sizeof(block_t));
+blockchain = (blockchain_t *) malloc(sizeof(blockchain_t));
+blockchain->chain = llist_create(MT_SUPPORT_TRUE);
+if (llist_add_node(blockchain->chain, genesis, ADD_NODE_FRONT) != 0)
+return (NULL);
+return (blockchain);
 }
