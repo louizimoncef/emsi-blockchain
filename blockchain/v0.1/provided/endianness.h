4f3a8bad8718c0e051658bb8e15d3db077b4b226
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-20 03:17:02 +0000

    Update endianness.h

--- blockchain/v0.1/provided/endianness.h
+++ blockchain/v0.1/provided/endianness.h
@@ -1,29 +1,31 @@
 #ifndef _ENDIANNESS_H_
 # define _ENDIANNESS_H_
+
 # include <stdint.h>
 # include <stddef.h>
+
 /*
  * # include <stdint.h>
  *
- * # define SWAPENDIAN_16(value)	(((value & 0xFF00) >> 8) | \
- *				((value & 0x00FF) << 8))
+ * # define SWAPENDIAN_16(value)(((value & 0xFF00) >> 8) | \
+ *((value & 0x00FF) << 8))
  *
- * # define SWAPENDIAN_32(value)	(((value & 0xFF000000ul) >> 24) | \
- *				((value & 0x00FF0000ul) >>  8) | \
- *				((value & 0x0000FF00ul) <<  8) | \
- *				((value & 0x000000FFul) << 24))
+ * # define SWAPENDIAN_32(value)(((value & 0xFF000000ul) >> 24) | \
+ *((value & 0x00FF0000ul) >>  8) | \
+ *((value & 0x0000FF00ul) <<  8) | \
+ *((value & 0x000000FFul) << 24))
  *
- * # define SWAPENDIAN_64(value)	(((value & 0xFF00000000000000ULL) >> 56) | \
- *				((value & 0x00FF000000000000ULL) >> 40) | \
- *				((value & 0x0000FF0000000000ULL) >> 24) | \
- *				((value & 0x000000FF00000000ULL) >>  8) | \
- *				((value & 0x00000000FF000000ULL) <<  8) | \
- *				((value & 0x0000000000FF0000ULL) << 24) | \
- *				((value & 0x000000000000FF00ULL) << 40) | \
- *				((value & 0x00000000000000FFULL) << 56))
+ * # define SWAPENDIAN_64(value)(((value & 0xFF00000000000000ULL) >> 56) | \
+ *((value & 0x00FF000000000000ULL) >> 40) | \
+ *((value & 0x0000FF0000000000ULL) >> 24) | \
+ *((value & 0x000000FF00000000ULL) >>  8) | \
+ *((value & 0x00000000FF000000ULL) <<  8) | \
+ *((value & 0x0000000000FF0000ULL) << 24) | \
+ *((value & 0x000000000000FF00ULL) << 40) | \
+ *((value & 0x00000000000000FFULL) << 56))
  */
 
-# define SWAPENDIAN(value)	_swap_endian((char *)(&value), sizeof(value))
+# define SWAPENDIAN(value)_swap_endian((char *)(&value), sizeof(value))
 
 /**
  * union _endian_u - Used to test endianness
@@ -33,10 +35,11 @@
  */
 typedef union _endian_u
 {
-	uint32_t value;
-	uint8_t bytes[4];
+uint32_t value;
+uint8_t bytes[4];
 } _endian_t;
 
-uint8_t	_get_endianness(void);
+uint8_t _get_endianness(void);
 void _swap_endian(void *p, size_t size);
+
 #endif /* ! _ENDIANNESS_H_ */
