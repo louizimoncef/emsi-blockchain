dcf504e85e24da9a10d24d3f94bf96de11d88580
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-20 01:29:27 +0000

    Update blockchain_serialize.c

--- blockchain/v0.1/blockchain_serialize.c
+++ blockchain/v0.1/blockchain_serialize.c
@@ -1,12 +1,12 @@
 #include "blockchain.h"
 
 
-int write_block(llist_node_t ptr, unsigned int no, void *arg){
+int write_block(llist_node_t ptr, unsigned int idx, void *arg){
 
 	FILE *fp;
 	block_t *block = ptr;
 
-	no=0;
+	idx=0;
 
 	if (!arg || !ptr) return -1;
 
