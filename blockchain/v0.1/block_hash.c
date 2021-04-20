4cc3859ebdb8f979b4bbf3f26be75e558df38604
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-20 01:05:16 +0000

    Update block_hash.c

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -9,7 +9,7 @@ uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH]
 {
 size_t length = block->data.len + sizeof(block->info);
 if (block)
-if (sha256((int8_t const *)block, length, hash_buf))
+if (sha256( (int8_t const *)block, length, hash_buf))
 return (hash_buf);
 return (NULL);
 }
