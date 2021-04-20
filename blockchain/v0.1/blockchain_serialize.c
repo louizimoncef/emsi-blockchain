5bfee37a79277d293a4a30cf79c1ef56037ec996
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-20 03:57:29 +0000

    Update blockchain_serialize.c

--- blockchain/v0.1/blockchain_serialize.c
+++ blockchain/v0.1/blockchain_serialize.c
@@ -1,37 +1,36 @@
 #include "blockchain.h"
 
 /**
- * write_block - a function that serializes a Blockchain into a file
- * @ptr: pointer to the block to be serialized
- * @idx: index of the parameter
- * @arg: The file in which the data will be written
- * Return: 0 upon success, or -1 upon failure
+ * block_to_file - write block fileds into passed file
+ * @blockPtr: pointer to the block 
+ * @no: index of the block unsed in my  case
+ * @file: The file to be stored into 
+ * Return: 0  success, -1  failure
  */
-int block_to_file(llist_node_t ptr, unsigned int no, void *file)
+int block_to_file(llist_node_t blockPtr, unsigned int no, void *file)
 {
-block_t *block = ptr;
+block_t *block = blockPtr;
 FILE *fptr;
 no = -1;
 if (file) 
 {
-fptr=(FILE*)file;
+fptr=(FILE *)file;
 fwrite((void *)&block->info, sizeof(block->info), 1, fptr);
 fwrite((void *)&block->data.len, sizeof(block->data.len), 1, fptr);
 fwrite(block->data.buffer, block->data.len, 1, fptr);
 fwrite(block->hash, sizeof(block->hash), 1, fptr);
-return no;
+return (no);
 }
-return 0;
+return (0);
 }
 /**
- * blockchain_serialize - a function that serializes a Blockchain into a file
- * @blockchain: pointer to blockchain to be serialized
- * @path: the path to a file to serialize the Blockchain into
+ * blockchain_serialize -  serializes a Blockchain and save it into file
+ * @blockchain: blockchain pointer
+ * @path: the file directorie
  *
- * Return: 0 upon success, or -1 upon failure
+ * Return: 0  success, -1  failure
  */
-
-int blockchain_serialize(blockchain_t const *blockchain, char const *path) 
+int blockchain_serialize(blockchain_t const *blockchain, char const *path)
 {
 uint8_t hblk_magic[4];
 uint8_t hblk_version[3];
@@ -43,7 +42,7 @@ return (-1);
 if (blockchain && path)
 {
 hblk_blocks = llist_size(blockchain->chain);
-if (hblk_blocks == -1) 
+if (hblk_blocks == -1)
 return (-1);
 memcpy(hblk_magic, "HBLK", 4);
 memcpy(hblk_version, "0.1", 3);
@@ -55,9 +54,9 @@ fwrite(&hblk_blocks, sizeof(hblk_blocks), 1, fptr);
 if (llist_for_each(blockchain->chain, block_to_file, fptr))
 {
 fclose(fptr);
-return 0;
+return (0);
 }
 }
 fclose(fptr);
-return -1;
+return (-1);
 }
