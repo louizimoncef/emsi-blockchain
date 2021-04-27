716884727ff2fd234b986b0cf8097a50a31df247
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-27 00:23:02 +0000

    Update blockchain_deserialize.c

--- blockchain/v0.1/blockchain_deserialize.c
+++ blockchain/v0.1/blockchain_deserialize.c
@@ -1,95 +1,118 @@
 #include "blockchain.h"
+void _blockchain_print(blockchain_t const *blockchain);
 
-
-void convert_endianness(block_t *block){
-
-	_swap_endian(&block->info.index, sizeof(block->info.index));
-	_swap_endian(&block->info.difficulty, sizeof(block->info.difficulty));
-	_swap_endian(&block->info.timestamp, sizeof(block->info.timestamp));
-	_swap_endian(&block->info.nonce, sizeof(block->info.nonce));
+/**
+ * block_from_file_no_endn - read block from the file
+ * @fptr: The file to be read from
+ * Return: block  success, NULL  failure
+ */
+block_t *block_from_file_no_endn(FILE *fptr)
+{
+block_t *block;
+if (fptr)
+{
+block = (block_t *) malloc(sizeof(block_t));
+if (!block)
+{
+return (NULL);
 }
-
-
-int read_blocks(FILE *fp, uint8_t swap_endian, blockchain_t *blockchain, int size){
-
-	int i;
-	block_t *block;
-	uint32_t data_len;
-	long int genesis_size;
-
-	genesis_size = sizeof(block->info) + sizeof(block->hash) + 20L;
-	fseek(fp, genesis_size, SEEK_CUR);
-
-	for (i = 0; i < size - 1; ++i){
-
-		block = malloc(sizeof(*block));
-
-		if (!block) return -1;
-
-		fread(&block->info, sizeof(block->info), 1, fp);
-		fread(&data_len, sizeof(data_len), 1, fp);
-
-		if (swap_endian)
-			_swap_endian(&data_len, sizeof(data_len));
-
-		fread(&block->data.buffer, data_len, 1, fp);
-		fread(&block->hash, SHA256_DIGEST_LENGTH, 1, fp);
-
-		if (swap_endian)
-			convert_endianness(block);
-
-		block->data.len = data_len;
-		*(block->data.buffer + data_len) = '\0';
-		llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
-	}
-
-	return 0;
+fread((void *)&block->info.index, sizeof(block->info.index), 1, fptr);
+fread((void *)&block->info.difficulty, sizeof(block->info.difficulty),
+1, fptr);
+fread((void *)&block->info.timestamp, sizeof(block->info.timestamp), 1, fptr);
+fread((void *)&block->info.nonce, sizeof(block->info.nonce), 1, fptr);
+fread((void *)&block->data.len, sizeof(block->data.len), 1, fptr);
+fread(block->data.buffer, block->data.len, 1, fptr);
+fread(block->hash, sizeof(block->hash), 1, fptr);
+return (block);
 }
-
-
-
-blockchain_t *blockchain_deserialize(char const *path){
-
-	FILE *fp;
-	hblk_file_t header;
-	blockchain_t *blockchain;
-	int size;
-	uint8_t swap_endian;
-
-	if (!path) return NULL;
-
-	if (access(path, R_OK) == -1) return NULL;
-
-	fp = fopen(path, "r");
-
-	if (!fp) return NULL;
-
-	fread(&header, sizeof(header), 1, fp);
-
-	if (memcmp(header.hblk_magic, "HBLK", 4) || memcmp(header.hblk_version, "0.1", 3)){
-
-		fclose(fp)
-
-		return NULL;
-	}
-		
-
-	blockchain = blockchain_create();
-
-	if (!blockchain){ 
-		fclose(fp)
-		return NULL;
-	}
-
-	swap_endian = _get_endianness() != header.hblk_endian;
-	size = header.hblk_blocks;
-
-	if (swap_endian)
-		_swap_endian(&size, sizeof(size));
-	if (read_blocks(fp, swap_endian, blockchain, size) == -1)
-		return (blockchain_destroy(blockchain), fclose(fp), NULL);
-
-	fclose(fp);
-
-	return blockchain;
+return (NULL);
+}
+/**
+ * block_from_file_endn - read block from the file
+ * with swaping_endian
+ * @fptr: The file to be read from
+ * Return: block  success, NULL  failure
+ */
+block_t *block_from_file_endn(FILE *fptr)
+{
+block_t *block;
+if (fptr)
+{
+block = (block_t *) malloc(sizeof(block_t));
+if (block)
+return (NULL);
+fread((void *)&block->info.index, sizeof(block->info.index), 1, fptr);
+fread((void *)&block->info.difficulty,
+sizeof(block->info.difficulty), 1, fptr);
+fread((void *)&block->info.timestamp, sizeof(block->info.timestamp), 1, fptr);
+fread((void *)&block->info.nonce, sizeof(block->info.nonce), 1, fptr);
+fread((void *)&block->data.len, sizeof(block->data.len), 1, fptr);
+fread(block->data.buffer, block->data.len, 1, fptr);
+fread(block->hash, sizeof(block->hash), 1, fptr);
+_swap_endian(&block->hash, sizeof(block->hash));
+_swap_endian(&block->data.len, sizeof(block->data.len));
+_swap_endian(&block->info.index, sizeof(block->info.index));
+_swap_endian(&block->info.difficulty, sizeof(block->info.difficulty));
+_swap_endian(&block->info.timestamp, sizeof(block->info.timestamp));
+_swap_endian(&block->info.nonce, sizeof(block->info.nonce));
+return (block);
+}
+return (NULL);
+}
+/**
+ * blockchain_deserialize -  load a Blockchain from the file
+ * @path: the file directorie
+ *
+ * Return: Blockchain  success, NULL  failure
+ */
+blockchain_t *blockchain_deserialize(char const *path)
+{
+uint8_t hblk_magic[4];
+uint8_t hblk_version[3];
+uint8_t hblk_endian;
+int32_t hblk_blocks;
+block_t * (*fun_ptr)(FILE *) = &block_from_file_no_endn;
+blockchain_t  *blockchain =  (blockchain_t *) malloc(sizeof(blockchain_t));
+block_t *block = NULL;
+int i;
+FILE *fptr = fopen(path, "r+");
+blockchain->chain =  llist_create(MT_SUPPORT_TRUE);
+if (!fptr)
+return (NULL);
+fread(hblk_magic, sizeof(hblk_magic), 1, fptr);
+fread(hblk_version, sizeof(hblk_version), 1, fptr);
+fread(&hblk_endian, sizeof(hblk_endian), 1, fptr);
+fread(&hblk_blocks, sizeof(hblk_blocks), 1, fptr);
+if (strcmp((const char *)hblk_magic, (const char *)"HBLK") &&
+strcmp((const char *)hblk_version, (const char *)"0.1"))
+{
+fclose(fptr);
+return (NULL);
+}
+if (_get_endianness() != hblk_endian)
+fun_ptr = &block_from_file_endn;
+for (i = 0 ; i < hblk_blocks ; i++)
+{
+block = (fun_ptr)(fptr);
+if (block)
+{
+if (llist_add_node(blockchain->chain, block,  ADD_NODE_REAR) == -1)
+{
+if (blockchain->chain)
+blockchain_destroy(blockchain);
+fclose(fptr);
+return (NULL);
+}
+}
+else
+{
+if (blockchain->chain)
+blockchain_destroy(blockchain);
+fclose(fptr);
+return (NULL);
+}
+}
+fclose(fptr);
+return (blockchain);
 }
