1230e02ae12139d5622b3cb7c8196310a61f292b
louizimoncef <moncefloui@gmail.com>
2021-04-27 22:15:04 +0000

    mining

--- blockchain/v0.2/hash_matches_difficulty.c
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -1,16 +1,23 @@
 #include "blockchain.h"
-uint32_t leadingZeroCalculer(uint8_t const *hash,size_t len)
+/**
+ * leadingZeroCalculer - calculute number of leading  zero in
+ * the chain bits of hash
+ * @hash: lhe hash
+ * @len: The lenght de hash
+ * Return: number of leadingZero  success, -1  failure
+ */
+uint32_t leadingZeroCalculer(uint8_t const *hash, size_t len)
 {
 uint8_t n, i = 0, x;
 uint32_t res = 0;
-if(hash)
+if (hash)
 {
-for(i = 0; i < len ; i++)
+for (i = 0; i < len ; i++)
 {
-x=hash[i];
-for(n=0; n < 8; n++)
+x = hash[i];
+for (n = 0; n < 8; n++)
 {
-if((x & 0x80) != 0)
+if ((x & 0x80) != 0)
 {
 return (res);
 }
@@ -21,7 +28,15 @@ res++;
 }
 return (-1);
 }
-int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH], uint32_t difficulty)
+/**
+ * hash_matches_difficulty - check if difficulty matches
+ * the  hash
+ * @hash: The hash
+ * @difficulty: The difficulty
+ * Return: 0  success, -1  failure
+ */
+int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
+uint32_t difficulty)
 {
-return (leadingZeroCalculer(hash, SHA256_DIGEST_LENGTH) == difficulty ? 1:0);
+return (leadingZeroCalculer(hash, SHA256_DIGEST_LENGTH) == difficulty ? 1 : 0);
 }
