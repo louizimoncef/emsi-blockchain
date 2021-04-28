dceb13d4aecc0825bbb9ef971e51903f5ccd3302
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-28 00:53:12 +0000

    Update block_is_valid.c

--- blockchain/v0.2/block_is_valid.c
+++ blockchain/v0.2/block_is_valid.c
@@ -1,48 +1,57 @@
 #include "blockchain.h"
-
-
-static int verify_genesis_block(block_t const *block){
-
-	blockchain_t *blockchain = blockchain_create();
-	int ret = 0;
-
-	if (!blockchain) return -1;
-
-	ret = memcmp(block, llist_get_head(blockchain->chain), sizeof(*block)) != 0;
-
-	blockchain_destroy(blockchain);
-
-	return (hash_matches_difficulty(block->hash,block->info->difficulty));
+/**
+ * is_genesis - checck if the block is genesis
+ * @block: The file to be read from
+ * Return: 0  success, -1  failure
+ */
+int is_genesis(block_t const *block)
+{
+blockchain_t *blockchain = blockchain_create();
+if (!blockchain)
+return (-1);
+if (memcmp(block, llist_get_head(blockchain->chain), sizeof(*block)) != 0)
+{
+blockchain_destroy(blockchain);
+return (-1);
 }
-
-
-static int verify_blocks(block_t const *block, block_t const *prev_block){
-
-	uint8_t block_sha[SHA256_DIGEST_LENGTH];
-	uint8_t prev_sha[SHA256_DIGEST_LENGTH];
-
-	if (prev_block->info.index != block->info.index - 1) return -1;
-
-	if (!block_hash(prev_block, prev_sha) || memcmp(prev_block->hash, prev_sha, SHA256_DIGEST_LENGTH)) return -1;
-
-
-	if (!block_hash(block, block_sha) ||
-		memcmp(block->hash, block_sha, SHA256_DIGEST_LENGTH) ||
-		memcmp(block->info.prev_hash, prev_sha, SHA256_DIGEST_LENGTH))
-		return -1;
-
-	if (block->data.len > BLOCKCHAIN_DATA_MAX) return -1;
-
-	return (hash_matches_difficulty(block->hash,block->info->difficulty);
+blockchain_destroy(blockchain);
+return (hash_matches_difficulty(block->hash, block->info.difficulty));
 }
-
-
-
-int block_is_valid(block_t const *block, block_t const *prev_block){
-
-	if (!block) return -1;
-
-	if (!prev_block) return (verify_genesis_block(block));
-
-	return verify_blocks(block, prev_block);
+/**
+ * verify_blocks - verify the validity of a block
+ * @block: The file to be read from
+ * @prev_block: The file to be read from
+ * Return: 0  success, -1  failure
+ */
+int verify_blocks(block_t const *block, block_t const *prev_block)
+{
+uint8_t block_sha[SHA256_DIGEST_LENGTH];
+uint8_t prev_sha[SHA256_DIGEST_LENGTH];
+if (prev_block->info.index != block->info.index - 1)
+return (-1);
+if (is_genesis(prev_block) == -1)
+if (!block_hash(prev_block, prev_sha) ||
+memcmp(prev_block->hash, prev_sha, SHA256_DIGEST_LENGTH))
+return (-1);
+if (!block_hash(block, block_sha) ||
+memcmp(block->hash, block_sha, SHA256_DIGEST_LENGTH) ||
+memcmp(block->info.prev_hash, prev_block->hash, SHA256_DIGEST_LENGTH))
+return (-1);
+if (block->data.len > BLOCKCHAIN_DATA_MAX)
+return (-1);
+return (hash_matches_difficulty(block->hash, block->info.difficulty));
+}
+/**
+ * block_is_valid - check if the block is valid
+ * @block: The file to be read from
+ * @prev_block: The file to be read from
+ * Return: 0  success, -1  failure
+ */
+int block_is_valid(block_t const *block, block_t const *prev_block)
+{
+if (!block)
+return (-1);
+if (!prev_block && !block->info.index)
+return (is_genesis(block));
+return (verify_blocks(block, prev_block));
 }
