b04cecd12d1d9f7b21bcd88b99bbdb29a7f0ed50
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-28 16:44:28 +0000

    Update block_is_valid.c

--- blockchain/v0.2/block_is_valid.c
+++ blockchain/v0.2/block_is_valid.c
@@ -53,8 +53,8 @@ if (!block)
 return (-1);
 if (!prev_block && !block->info.index)
 return (is_genesis(block));
-if (hash_matches_difficulty(prev_block->hash,
-prev_block->info.difficulty) != 0)
+if (hash_matches_difficulty(block->hash,
+block->info.difficulty) != 0)
 return (-1);
 if (hash_matches_difficulty(prev_block->hash,
 prev_block->info.difficulty) != 0)
