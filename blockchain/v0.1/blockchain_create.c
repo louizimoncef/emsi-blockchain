37dd5947982fc279701175f0c4073e1333a82580
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-28 00:30:25 +0000

    Update blockchain_create.c

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -12,6 +12,7 @@ genesis = (block_t *) malloc(sizeof(block_t));
 memcpy(genesis, (block_t *) &gene, sizeof(block_t));
 blockchain = (blockchain_t *) malloc(sizeof(blockchain_t));
 blockchain->chain = llist_create(MT_SUPPORT_TRUE);
+block_hash(genesis, genesis->hash)
 if (llist_add_node(blockchain->chain, genesis, ADD_NODE_FRONT) != 0)
 return (NULL);
 return (blockchain);
