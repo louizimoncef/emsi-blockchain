3984bc32336a03b8f8ed006394a595c98818d333
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-28 01:55:44 +0000

    Update block_create.c

--- blockchain/v0.2/block_create.c
+++ blockchain/v0.2/block_create.c
@@ -1,5 +1,34 @@
 #include "blockchain.h"
 /**
+ * leadingZeroCalculer - calculute number of leading  zero in
+ * the chain bits of hash
+ * @hash: lhe hash
+ * @len: The lenght de hash
+ * Return: number of leadingZero  success, -1  failure
+ */
+uint32_t leadingZeroCalculer(uint8_t const *hash, size_t len)
+{
+uint8_t n, i = 0, x;
+uint32_t res = 0;
+if (hash)
+{
+for (i = 0; i < len ; i++)
+{
+x = hash[i];
+for (n = 0; n < 8; n++)
+{
+if ((x & 0x80) != 0)
+{
+return (res);
+}
+x = x << 1;
+res++;
+}
+}
+}
+return (-1);
+}
+/**
  * block_create - create a block and initialises it
  *@prev: previous  block
  *@data: data to be stored in the block
@@ -14,7 +43,6 @@ block_t *block = (block_t *) malloc(sizeof(block_t));
 if (!prev || !data)
 return (NULL);
 block->info.index = prev->info.index + 1;
-block->info.difficulty = 0;
 block->info.nonce = 0;
 block->info.timestamp = time(0);
 memcpy(block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
@@ -25,5 +53,6 @@ else
 block->data.len = data_len;
 for (i = 0; i < SHA256_DIGEST_LENGTH; i++)
 block->hash[i] = 0;
+block->info.difficulty = leadingZeroCalculer(block->hash,SHA256_DIGEST_LENGTH);
 return (block);
 }
