603e5312f3bc635e362839ff2f58f17dc7696351
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-29 01:30:53 +0000

    Update block_create.c

--- blockchain/v0.2/block_create.c
+++ blockchain/v0.2/block_create.c
@@ -10,13 +10,14 @@ block_t *block_create(block_t const *prev, int8_t const *data,
 uint32_t data_len)
 {
 uint32_t i;
+time_t sec = (time_t)2;
 block_t *block = (block_t *) malloc(sizeof(block_t));
 if (!prev || !data)
 return (NULL);
 block->info.index = prev->info.index + 1;
 block->info.difficulty = 0;
 block->info.nonce = 0;
-block->info.timestamp = time((time_t *)2);
+block->info.timestamp = time(sec);
 memcpy(block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
 memcpy(block->data.buffer, data, BLOCKCHAIN_DATA_MAX);
 if (data_len > (uint32_t)BLOCKCHAIN_DATA_MAX)
