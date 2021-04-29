e99ca8e62f4a0957ba70103589258a6d289bdf2d
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-29 00:37:11 +0000

    Update blockchain.h

--- blockchain/v0.2/blockchain.h
+++ blockchain/v0.2/blockchain.h
@@ -77,4 +77,5 @@ int block_is_valid(block_t const *block, block_t const *prev_block);
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH], uint32_t difficulty);
 uint8_t _get_endianness(void);
 void _swap_endian(void *p, size_t size);
+void block_mine(block_t *block);
 #endif 
