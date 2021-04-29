e5cb1d322a92cbd30f01ddba5d685d2bd40aee4b
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-29 00:35:04 +0000

    Create block_mine.c

---
+++ blockchain/v0.2/block_mine.c
@@ -0,0 +1,16 @@
+#include "blockchain.h"
+/**
+ * block_mine - mining by changing the nonce
+ * @block: the mined block
+ */
+void block_mine(block_t *block)
+{
+uint8_t blockHash[SHA256_DIGEST_LENGTH];
+block_hash(block, blockHash);
+while (!hash_matches_difficulty(blockHash, block->info.difficulty))
+{
+block->info.nonce++;
+block_hash(block, blockHash);
+}
+memcpy(block->hash, blockHash, SHA256_DIGEST_LENGTH);
+}
